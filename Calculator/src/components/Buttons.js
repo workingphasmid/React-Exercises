51f87067ae816aacadf285635142f28188ac6b6f
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-05-19 09:10:23 +0800

    Logic - Can input now.

--- Calculator/src/components/Buttons.js
+++ Calculator/src/components/Buttons.js
@@ -9,34 +9,38 @@ export default function Buttons({ children }) {
 // for dev purposes
 let nextId = 0;
 
-export function Left() {
+export function Left({ handleButtonClick }) {
   const specialOperators = ["AC", "+/-", "%"];
   const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
 
   return (
     <div className="left">
       {specialOperators.map((op) => (
-        <Button key={nextId++} value={op} type="specialOperator" />
+        <Button key={nextId++} value={op} type="specialOperator" handleButtonClick={handleButtonClick} />
       ))}
       {numbers.map((op) => (
-        <Button key={nextId++} value={op} type="number" />
+        <Button key={nextId++} value={op} type="number" handleButtonClick={handleButtonClick} />
       ))}
     </div>
   );
 }
 
-export function Right() {
+export function Right({ handleButtonClick }) {
   const operators = ["/", "x", "-", "+", "="];
 
   return (
     <div className="right">
       {operators.map((op) => (
-        <Button key={nextId++} value={op} type="operator" />
+        <Button key={nextId++} value={op} type="operator" handleButtonClick={handleButtonClick} />
       ))}
     </div>
   );
 }
 
-function Button({ value, type }) {
-  return <button className={`button--${value} button--${type}`}>{value}</button>;
+function Button({ value, type, handleButtonClick }) {
+  return (
+    <button className={`button--${value} button--${type}`} onClick={() => handleButtonClick(value)}>
+      {value}
+    </button>
+  );
 }
--- Calculator/src/components/Calculator.js
+++ Calculator/src/components/Calculator.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Buttons, { Left, Right } from "./Buttons";
 
 import "./Calculator.css";
 
 export function Calculator() {
+  const [expression, setExpression] = useState("");
+
+  function handleButtonClick(currentButton) {
+    setExpression(expression + currentButton);
+  }
+
   return (
     <div className="calculator">
-      <input type="text" className="calculator__input" />
+      <input type="text" className="calculator__input" value={expression} onChange={(e) => setExpression(e.target.value)} />
       <Buttons>
-        <Left />
-        <Right />
+        <Left handleButtonClick={handleButtonClick} />
+        <Right handleButtonClick={handleButtonClick} />
       </Buttons>
     </div>
   );
