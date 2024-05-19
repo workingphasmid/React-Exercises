4c529840ba33afffee5beea76655c450cc65d19f
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-05-19 08:07:49 +0800

    Design - initial design 1.0

--- Calculator/src/components/Buttons.js
+++ Calculator/src/components/Buttons.js
@@ -7,28 +7,36 @@ export default function Buttons({ children }) {
 }
 
 // for dev purposes
-let leftId = 0;
+let nextId = 0;
 
 export function Left() {
   const specialOperators = ["AC", "+/-", "%"];
-  const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3"];
+  const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
 
   return (
     <div className="left">
       {specialOperators.map((op) => (
-        <Button key={leftId++} value={op} type="specialOperator" />
+        <Button key={nextId++} value={op} type="specialOperator" />
       ))}
       {numbers.map((op) => (
-        <Button key={leftId++} value={op} type="number" />
+        <Button key={nextId++} value={op} type="number" />
       ))}
     </div>
   );
 }
 
 export function Right() {
-  return <div className="right">Right</div>;
+  const operators = ["/", "x", "-", "+", "="];
+
+  return (
+    <div className="right">
+      {operators.map((op) => (
+        <Button key={nextId++} value={op} type="operator" />
+      ))}
+    </div>
+  );
 }
 
 function Button({ value, type }) {
-  return <button className={`button--${type}`}>{value}</button>;
+  return <button className={`button--${value} button--${type}`}>{value}</button>;
 }
